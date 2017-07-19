5d4c2123d5d006c0aac40bf5d7eded11889aff31
Gerald Holmes <bytepair@gmail.com>
2017-07-18 22:11:28 -0400

    End of TicTacToe Tutorial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* custom components */
+import Game from './boardComponents/game';
+
+/* styling */
+import './css/index.css';
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
